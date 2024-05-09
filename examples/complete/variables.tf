c905171bc616c7d9106d9f79dbc5f2f080cec193
Bryce Lynn <bryce.lynn@nttdata.com>
2024-05-09 16:53:52 -0700

    add missing var types

--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -14,20 +14,24 @@
 
 variable "vpc_name" {
   default = "test-vpc-015935234"
+  type    = string
 }
 
 variable "vpc_cidr" {
   default = "10.1.0.0/16"
+  type    = string
 }
 
 variable "private_subnets" {
   description = "List of private subnet cidrs"
   default     = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
+  type        = list(string)
 }
 
 variable "availability_zones" {
   description = "List of availability zones for the VPC"
   default     = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  type        = list(string)
 }
 
 ### Namespace related variables
